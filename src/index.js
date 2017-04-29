214b437989203fbb5f8394eeeb1cbc5bebe2fd3a
Gordon Doskas <gordon2012@users.noreply.github.com>
2017-04-29 00:10:34 -0700

    Replace Spider prototype with ES6 class

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+/*
+Major Refactor:
+
+TODO:
+- Restructure folders
+- Move asset definitions to some kind of manifest
+
+- Replace prototype inheritance with ES6 classes
+- Hero
+DOING:
+
+DONE:
+- Spider
+
+
+*/
+
 // require('file-loader?name=[name].[ext]!./index.html'); // WIP
 // Hero
 //
@@ -64,47 +81,51 @@ Hero.prototype._getAnimationName = function() {
         // Running
         name = 'run';
     }
-
     return name;
 }
 
 
 // Spider
 //
-function Spider(game, x, y) {
-    Phaser.Sprite.call(this, game, x, y, 'spider');
+const SPIDER_SPEED = 100;
+class Spider extends Phaser.Sprite {
+    constructor(game, x, y) {
+        super(game, x, y);
 
-    this.anchor.set(0.5);
+        Phaser.Sprite.call(this, game, x, y, 'spider');
 
-    this.animations.add('crawl', [0,1,2], 8, true);
-    this.animations.add('die', [0,4,0,4,0,4,3,3,3,3,3,3], 12);
-    this.animations.play('crawl');
+        this.anchor.set(0.5);
 
-    this.game.physics.enable(this);
-    this.body.collideWorldBounds = true;
-    this.body.velocity.x = Spider.SPEED;
-}
+        this.animations.add('crawl', [0,1,2], 8, true);
+        this.animations.add('die', [0,4,0,4,0,4,3,3,3,3,3,3], 12);
+        this.animations.play('crawl');
 
-Spider.SPEED = 100;
-
-Spider.prototype = Object.create(Phaser.Sprite.prototype);
-Spider.prototype.constructor = Spider;
-
-Spider.prototype.update = function() {
-    if(this.body.touching.right || this.body.blocked.right) {
-        this.body.velocity.x = -Spider.SPEED;
-    } else if(this.body.touching.left || this.body.blocked.left) {
+        this.game.physics.enable(this);
+        this.body.collideWorldBounds = true;
         this.body.velocity.x = Spider.SPEED;
     }
-};
 
-Spider.prototype.die = function() {
-    this.body.enable = false;
+    static get SPEED() {
+        return SPIDER_SPEED;
+    }
 
-    this.animations.play('die').onComplete.addOnce(function() {
-        this.kill();
-    }, this);
-};
+    update() {
+        if(this.body.touching.right || this.body.blocked.right) {
+            this.body.velocity.x = -Spider.SPEED;
+        } else if(this.body.touching.left || this.body.blocked.left) {
+            this.body.velocity.x = Spider.SPEED;
+        }
+    }
+
+    die() {
+        this.body.enable = false;
+
+        this.animations.play('die').onComplete.addOnce(function() {
+            this.kill();
+        }, this);
+    }
+
+}
 
 
 // Number of levels
@@ -138,7 +159,7 @@ PlayState.init = function(data) {
     // Player has key?
     this.hasKey = false;
 
-    this.level = (data.level || 0) % LEVEL_COUNT;
+    this.level = (data.level || 1) % LEVEL_COUNT;
 };
 
 PlayState.preload = function() {
